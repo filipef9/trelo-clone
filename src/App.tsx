3f5ed37c4bb87cf074cf90e87922be82d4342512
filipef9 <filipef9@hotmail.com>
2022-07-29 21:45:36 +0000

    :sparkles: add column component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import { AppContainer, CardContainer, ColumnContainer, ColumnTitle } from './styles';
+import { Column } from './Column';
+import { AppContainer, ColumnContainer } from './styles';
 
 export const App = () => {
   return (
     <AppContainer>
-      <ColumnContainer>
-        <ColumnTitle>Todo:</ColumnTitle>
-        <CardContainer>FirstItem</CardContainer>
-        <CardContainer>SecondItem</CardContainer>
-        <CardContainer>ThirdItem</CardContainer>
-      </ColumnContainer>
+      <Column text="Todo:" />
     </AppContainer>
   )
 }
